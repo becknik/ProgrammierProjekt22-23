215d377205f4193814774959ece3e38070517abe
jarnnk <jannikb@posteo.de>
2022-11-09 19:26:06 +0100

    Added simple parsing and node/edge detection

--- ProgrammingProject/src/main/java/loader/GraphReader.java
+++ ProgrammingProject/src/main/java/loader/GraphReader.java
@@ -1,52 +1,88 @@
 package ProgrammingProject.src.main.java.loader;
 
 import java.io.*;
-import java.nio.file.Path;
+import java.util.Optional;
 
 public class GraphReader {
 
-    public static void main(String[] args) {
 
+    private enum LineType {
+        NODE,
+        EDGE
+    }
+
+    private record ParsedLine(LineType type, Number[] values) {
+
+    }
+
+    public static void main(String[] args) {
         GraphReader.read(new File("stgtregbz.fmi"));
     }
 
     public static void read(final File file) {
 
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file)) {
 
-            GraphReader.prepareBuffer(bufferedReader);
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+
+            String line;
+            while (( line = bufferedReader.readLine() ) != null) {
+
+                Optional<ParsedLine> optionalParsedLine = GraphReader.prepareBuffer(line);
+                if (optionalParsedLine.isEmpty()) continue;
+
+                if (optionalParsedLine.get().type == LineType.NODE) {
+                    System.out.print("Adding Node to array: ");
+                    for (Number number : optionalParsedLine.get().values) {
+                        System.out.print(number + ", ");
+                    }
+                    System.out.println();
+                    GraphReader.parseNode(optionalParsedLine.get().values);
+                } else if (optionalParsedLine.get().type == LineType.EDGE) {
+                    GraphReader.parseEdge(optionalParsedLine.get().values);
+                } else continue;
+            }
 
         } catch (FileNotFoundException e) {
-            System.err.println("Reader could not find graph file location of " + file.toString());
+            System.err.println("Reader could not find graph file location of " + file);
             e.printStackTrace();
         } catch (IOException e) {
-            System.err.println("IO exception for accessing graph file" + file.toString());
+            System.err.println("IO exception for accessing graph file" + file);
             e.printStackTrace();
         }
     }
 
 
-    private static void prepareBuffer(final BufferedReader bufferedReader) throws IOException {
-        String buffer;
+    private static Optional<ParsedLine> prepareBuffer(final String line) {
 
-        while (( buffer = bufferedReader.readLine() ) != null) {
-            String[] parsedBuffer = buffer.trim().split(" ");
+            String[] rawValues = line.trim().split(" "); // TODO: Maybe use RegEx here?
 
-            System.out.println(parsedBuffer);
+            if (rawValues.length < 5) return Optional.empty();
+            else if (rawValues[4].equals("0")) {   // TODO this may cause problems
+                int nodeID = Integer.parseInt(rawValues[0]);    // Maybe chance this to long?
+                long idk = Long.parseLong(rawValues[1]);
+                double  longitude = Double.parseDouble(rawValues[2]);
+                double  latitude = Double.parseDouble(rawValues[3]);
 
-            if (parsedBuffer[4] == "0") {   // TODO this may cause problems
-                GraphReader.parseNodes(parsedBuffer);
-            } else if (Integer.parseInt(parsedBuffer[4]) > 0) {
-                GraphReader.parseEdges(parsedBuffer);
+                Number[] parsedNumbers = {nodeID, idk, longitude, latitude};
+                return Optional.of(new ParsedLine(LineType.NODE, parsedNumbers));
+
+            } else if (Integer.parseInt(rawValues[4]) > 0) {
+                int startNode = Integer.parseInt(rawValues[0]);
+                int targetNode = Integer.parseInt(rawValues[0]);
+                int distance = Integer.parseInt(rawValues[0]);
+
+                Number[] parsedNumbers = {startNode, targetNode, distance};
+                return Optional.of(new ParsedLine(LineType.NODE, parsedNumbers));
             }
-        }
+
+            return Optional.empty();
     }
 
-    private static  void parseNodes(String[] buffer) {
+    private static  void parseNode(Number[] buffer) {
 
     }
 
-    private static void parseEdges(String[] buffer){
+    private static void parseEdge(Number[] buffer){
 
     }
 
