7c546d2137e952d2f5915df8a7e8b56fb8c7f2d7
jarnnk <jannikb@posteo.de>
2022-11-09 15:43:12 +0100

    Started implementing parsing logic

--- ProgrammingProject/src/main/java/loader/GraphReader.java
+++ ProgrammingProject/src/main/java/loader/GraphReader.java
@@ -1,19 +1,52 @@
 package ProgrammingProject.src.main.java.loader;
 
+import java.io.*;
 import java.nio.file.Path;
 
 public class GraphReader {
 
-    public static void read(final Path path){
-        GraphReader.parseNodes();
-        GraphReader.parseEdges();
+    public static void main(String[] args) {
+
+        GraphReader.read(new File("stgtregbz.fmi"));
     }
 
-    private static void parseNodes(){
+    public static void read(final File file) {
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file)) {
+
+            GraphReader.prepareBuffer(bufferedReader);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Reader could not find graph file location of " + file.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("IO exception for accessing graph file" + file.toString());
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void prepareBuffer(final BufferedReader bufferedReader) throws IOException {
+        String buffer;
+
+        while (( buffer = bufferedReader.readLine() ) != null) {
+            String[] parsedBuffer = buffer.trim().split(" ");
+
+            System.out.println(parsedBuffer);
+
+            if (parsedBuffer[4] == "0") {   // TODO this may cause problems
+                GraphReader.parseNodes(parsedBuffer);
+            } else if (Integer.parseInt(parsedBuffer[4]) > 0) {
+                GraphReader.parseEdges(parsedBuffer);
+            }
+        }
+    }
+
+    private static  void parseNodes(String[] buffer) {
 
     }
 
-    private static void parseEdges(){
+    private static void parseEdges(String[] buffer){
 
     }
 
