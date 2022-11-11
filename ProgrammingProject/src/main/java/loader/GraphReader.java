d2ea6e07647398c138a97046da3b5d2c3205ee7e
jarnnk <jannikb@posteo.de>
2022-11-12 00:41:06 +0100

    Various code cleanups, fixed gotchas, added commentary & JavaDoc, implemented print-out of AdjacencyGraph object

--- ProgrammingProject/src/main/java/loader/GraphReader.java
+++ ProgrammingProject/src/main/java/loader/GraphReader.java
@@ -3,42 +3,22 @@ package ProgrammingProject.src.main.java.loader;
 import ProgrammingProject.src.main.java.struct.AdjacencyGraph;
 
 import java.io.*;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 public class GraphReader {
     private static final Logger logger = Logger.getLogger(GraphReader.class.getName());
     public static boolean enableLogging;
 
+    public static void main(String[] args) throws FileNotFoundException {
+        // Debugging:
+        //GraphReader.enableLogging = true;
+        //AdjacencyGraph.enableLogging = true;
 
-    private enum LineType {
-        NODE,
-        EDGE
-    }
-
-    /**
-     * Simple struct for wrapping a {@code LineType} with values of a pgrah node or edge
-     * @param type - (Optional) Used to determine if node or edge
-     * @param values - A array of graph Numbers which follow no specific order...
-     *               TODO due to records no supporting inheritance wrapping might be a bad idea
-     */
-    private record ParsedLine(LineType type, Number[] values) {
-        ParsedLine(Number[] values) {   // Maybe useless
-            this(values.length == 4 ? LineType.NODE : LineType.EDGE, values);
-        }
-        @Override
-        public String toString() {
-            if (type == LineType.NODE) {
-                return String.format("%s:\tnode ID: %d,\tidk %d,\tlongitude: %f,\tlatitude: %f%n", type.toString(), values[0], values[1], values[2], values[3]);
-            } else {
-                return String.format("%s:\tstart node ID: %d,\ttarget node ID: %d,\t idk: %d%n", type.toString(), values[0], values[1], values[2]);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        enableLogging = true;
-        GraphReader.read(new File("stgtregbz.fmi"));
+        // Testing:
+        File file = new File("stgtregbz.fmi");
+        AdjacencyGraph testAdjList = GraphReader.read(file);
+        //testAdjList.printOutStructs(System.out);
+        testAdjList.printOutStructs(new PrintStream(new PrintStream(file.getName() + "-AdjacencyGraph.txt")));
     }
 
     /**
@@ -46,51 +26,47 @@ public class GraphReader {
      * @param file - The file of the raw graph contents
      */
     public static AdjacencyGraph read(final File file) {
+        int nodeCount;
+        int edgeCount;
+        AdjacencyGraph adjGraph = null;
 
-        int nodeCount = 0;
-        int edgeCount = 0;
-        AdjacencyGraph adjGraph;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
 
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            // Skips the first irrelevant 5 lines
+            for (int i = 0; i < 5; i++) bufferedReader.readLine();
 
-            String line = "";
-
-            // Skips the first 5 lines
-            for(i=0; i<5; i++){
-                bufferedReader.readLine();
-            }
-
-            // Reads the number of nodes and edges
+            // Reads number of nodes and edges
             nodeCount = Integer.parseInt(bufferedReader.readLine());
             edgeCount = Integer.parseInt(bufferedReader.readLine());
 
             adjGraph = new AdjacencyGraph(nodeCount, edgeCount);
 
-            // Loops through every node line and adds node to adjacency graph
-            for(int nodeId = 0; nodeId<nodeCount; nodeId++){
-
+            // Parses and adds every node line String and adds node to adjacency graph object
+            for (int nodeId = 0; nodeId < nodeCount; nodeId++) {
+                line = bufferedReader.readLine();
                 String[] rawValues = line.trim().split(" ");
 
-                long idk = Long.parseLong(rawValues[1]);
                 double  latitude = Double.parseDouble(rawValues[2]);
                 double  longitude = Double.parseDouble(rawValues[3]);
 
+                // Logging
+                if (AdjacencyGraph.enableLogging) GraphReader.logger.info(String.format("Trying to add node no. %d\t with latitude %f,\t longitude %f\t to adjacency arrays.%n", nodeId, latitude, longitude));
                 adjGraph.addNode(nodeId, longitude, latitude);
             }
 
-            // Loops through every edge line and adds edge to adjacency graph
-            for (int edgeId = 0; edgeId<edgeCount; edgeId++){
-
+            // Parses and adds every edge line String and adds edge to adjacency graph
+            for (int edgeId = 0; edgeId < edgeCount; edgeId++){
+                line = bufferedReader.readLine();
                 String[] rawValues = line.trim().split(" ");
 
-                int startNode = Integer.parseInt(rawValues[0]);
+                int sourceNode = Integer.parseInt(rawValues[0]);
                 int targetNode = Integer.parseInt(rawValues[1]);
-                int distance = Integer.parseInt(rawValues[2]);
 
-                adjGraph.addEdge(edgeId, startNode, targetNode);
+                // Logging
+                if (AdjacencyGraph.enableLogging) GraphReader.logger.info(String.format("Trying to add edge no. %d\t with source node no %d,\t target node no %d\t to adjacency arrays.%n", edgeId, sourceNode, targetNode));
+                adjGraph.addEdge(edgeId, sourceNode, targetNode);
             }
-
-
         } catch (FileNotFoundException e) {
             System.err.println("Reader could not find graph file location of " + file);
             e.printStackTrace();
@@ -98,53 +74,6 @@ public class GraphReader {
             System.err.println("IO exception for accessing graph file" + file);
             e.printStackTrace();
         }
+        return adjGraph;
     }
-
-    /**
-     * Extracts the input Strings of the raw graph files and parses them to data types. Also figures somehow out which
-     * kind of line {@code LineType} the corresponding line is and wrapps all information in a {@code ParsedLine} object
-     * @param line - The string to be parsed and typed
-     * @return - The {@code ParsedLine} record object
-     */
-    private static Optional<ParsedLine> prepareBuffer(final String line) {
-
-            String[] rawValues = line.trim().split(" "); // TODO: Maybe use RegEx here?
-
-            if (rawValues.length < 5) {
-                if (enableLogging) logger.info("No valid node or edge detected");
-                return Optional.empty();
-            } else if (rawValues[4].equals("0")) {   // TODO this may cause problems
-                int nodeID = Integer.parseInt(rawValues[0]);    // Maybe chance this to long?
-                long idk = Long.parseLong(rawValues[1]);
-                double  longitude = Double.parseDouble(rawValues[2]);
-                double  latitude = Double.parseDouble(rawValues[3]);
-
-                return Optional.of(new ParsedLine(LineType.NODE, new Number[]{nodeID, idk, longitude, latitude}));
-            } else {
-                int startNode = Integer.parseInt(rawValues[0]);
-                int targetNode = Integer.parseInt(rawValues[0]);
-                int distance = Integer.parseInt(rawValues[0]);
-
-                return Optional.of(new ParsedLine(LineType.EDGE, new Number[]{startNode, targetNode, distance}));
-            }
-    }
-
-    /**
-     * Outputs the formatted contents of the {@code ParsedLine} into the info log, when the {@code enableLogging}
-     * variable is set.
-     * @param parsedLine - The contents to be formatted & logged
-     */
-    private static void logParsedLine(final ParsedLine parsedLine) {
-        if (GraphReader.enableLogging) {
-            logger.info("Adding" + parsedLine.toString());
-        }
-    }
-    private static  void parseNode(Number[] buffer) {
-
-    }
-
-    private static void parseEdge(Number[] buffer){
-
-    }
-
 }
--- ProgrammingProject/src/main/java/struct/AdjacencyGraph.java
+++ ProgrammingProject/src/main/java/struct/AdjacencyGraph.java
@@ -1,52 +1,97 @@
 package ProgrammingProject.src.main.java.struct;
 
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class AdjacencyGraph extends Graph{
+    private static final Logger logger = Logger.getLogger(AdjacencyGraph.class.getName());
+    public static boolean enableLogging;
 
-    private double[] longitudes;
-    private double[] latitudes;
+    // Node stuff
+    private final double[] longitudes;
+    private final double[] latitudes;
 
-    private int[] sources;
-    private int[] targets;
-    private int[] offset;
+    // Edge stuff
+    private final int[] sources;
+    private final int[] targets;
+    private final int[] offset;
     private int cachedSourceNode;
-    private double[] distances;
+
+    // Stuff to be calculated:
+    private final double[] distances;
 
     public AdjacencyGraph(int nodeCount, int edgeCount){
-
         longitudes = new double[nodeCount];
         latitudes = new double[nodeCount];
-        offset = new int[nodeCount];
+        offset = new int[nodeCount+1];  // Gotcha!
         sources = new int[edgeCount];
         targets = new int[edgeCount];
         distances = new double[edgeCount];
     }
 
+    /**
+     * Simply adds some node by specifying the nodes ID, longitude & latitude
+     * @param nodeId - Number of occurrence in the graph file
+     * @param longitude
+     * @param latitude
+     */
     public void addNode(int nodeId, double longitude, double latitude){
         longitudes[nodeId] = longitude;
         latitudes[nodeId] = latitude;
     }
 
+    /**
+     * TODO
+     * @param edgeId
+     * @param source
+     * @param target
+     */
     public void addEdge(int edgeId, int source, int target){
         sources[edgeId] = source;
         targets[edgeId] = target;
 
-        if (cachedSourceNode + 1 < source) {    // Nodes without outgoing nodes, are detected here
-            /*
-            Adding offset value of the last node with outgoing nodes to the nodes without outgoing nodes.
-            Due to offset being set on the next nodes offset entry, the for loop starts with 1 for the next node and adds
-            1 because the offset value of the next node matters.
-             */
-            for (int i = 1; i <= source - cachedSourceNode; i++) {
-                offset[cachedSourceNode + i + 1] = offset[cachedSourceNode + 1];
+        if (cachedSourceNode <= source - 1) {    // Nodes without outgoing nodes, are caught right here
+            // Adding offset value of (last node with outgoing nodes) to the following nodes without outgoing nodes
+            // Due to offset of node v being set on the next nodes (v+1) offset entry
+            // The for loop starts with 2 for the next and following nodes (v+1) offset entry (v+2+k) without outgoing nodes
+            for (int i = 2; i <= source - cachedSourceNode; i++) {
+                offset[cachedSourceNode + i] = offset[cachedSourceNode + 1];
+                // Logging
+                if (AdjacencyGraph.enableLogging) AdjacencyGraph.logger.info(String.format("Setting offset of node %d (=%d) to offset of node %d (%d)",
+                        cachedSourceNode + i, offset[cachedSourceNode + i + 1], cachedSourceNode, offset[cachedSourceNode + 1]));
             }
             this.cachedSourceNode = source;
         }
-        if (cachedSourceNode == source) {
-            offset[source + 1] = offset[source];
-            cachedSourceNode = source;
+        offset[source + 1]++;
+    }
+
+    /**
+     * Prints out the current objects structure with all interesting values in a formatted tabular
+     * @param out - PrintStream to be printed to
+     */
+    public void printOutStructs(final PrintStream out) {
+        out.print(" Node ID/Index:\t| Latitude:\t| Longitude:\t| Targets:\n");
+        for (int i = 0; i < this.longitudes.length; i++) {
+            // TODO Add distance value to outgoing nodes
+            out.printf("  %d\t\t|  %f\t|  %f\t|  %s%n", i, latitudes[i], longitudes[i], Arrays.toString(getOutgoingNodes(i)));
         }
-        offset[source+1]++;
+    }
+
+    /**
+     * Returns the outgoing nodes of a node specified via node ID alias the latitude/ longitudes corresponding index
+     * @param node - The nodes ID/ index of which the outgoing nodes are requested
+     * @return - The outgoing nodes typed as int[]
+     */
+    private int[] getOutgoingNodes(final int node) {
+        int startOutgoingNodeID = offset[node];
+        int exclusiveOutgoingNodeID = offset[node + 1];
+        return Arrays.copyOfRange(targets, startOutgoingNodeID, exclusiveOutgoingNodeID);
+    }
+
+    public void calculateDistances() { //TODO
+        if (distances[(int) (Math.random() * distances.length)] != 0) {
+            throw new RuntimeException("Trying to calculate the distances again? HOW DARE YOU?!");
+        }
     }
 }
