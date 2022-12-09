563e05dc35d955cb75a90c11baca1ba0cfa23a20
jarnnk <jannikb@posteo.de>
2022-12-10 00:47:07 +0100

    Created test classes, refactored main, adjusted logging for testing methods

---
+++ src/main/java/test/DijkstraTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import loader.GraphReader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import struct.AdjacencyGraph;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class DijkstraTest {
+
+	static final int TEST_REPETITIONS = 3;  // TODO This is ugly
+
+	static GraphFileType graphFileType;
+	AdjacencyGraph adjGraph;
+
+	@BeforeAll
+	public static void setUp () {
+		DijkstraTest.graphFileType = GraphFileType.GERMANY;  // TODO set this one into the run config
+
+		//GraphReader.enableLogging = true;
+		//AdjacencyGraph.enableLogging = true;
+	}
+
+	@BeforeEach
+	public void prepareAdjGraph () {
+		File file = new File(DijkstraTest.graphFileType.fileName);
+		this.adjGraph = GraphReader.createAdjacencyGraphOf(file);
+	}
+
+	@Tag("Benchmark")
+	@ParameterizedTest(name = "oneToAllBenchmark{0}")
+	@ValueSource(ints = {3, 42})
+	public void oneToOneBenchmark (int sourceNode) {
+		int targetNode = 0;     // TODO How to do test parameterization with multiple parameters?
+
+		long oneToOneDijkstraStart = System.currentTimeMillis();
+		int[] thisIsTheWay = this.adjGraph.oneToOneDijkstra(sourceNode, targetNode);
+		long oneToOneDijkstraEnd = System.currentTimeMillis();
+
+		long oneToOneDijkstraElapsedTime = oneToOneDijkstraEnd - oneToOneDijkstraStart;
+
+		Logging.logTestBenchmark(DijkstraTest.graphFileType, "oneToOne", oneToOneDijkstraElapsedTime);
+		System.out.println("The path from node" + sourceNode + " to " + targetNode + ":\n" + Arrays.toString(thisIsTheWay));
+	}
+
+	@Tag("Benchmark")
+	@RepeatedTest(TEST_REPETITIONS)
+	public void oneToAllBenchmark () {
+		long oneToAllDijkstraStart = System.currentTimeMillis();
+		this.adjGraph.oneToAllDijkstra(12323);
+		long oneToAllDijkstraEnd = System.currentTimeMillis();
+
+		long oneToAllDijkstraElapsedTime = oneToAllDijkstraEnd - oneToAllDijkstraStart;
+
+		Logging.logTestBenchmark(DijkstraTest.graphFileType, "oneToAll", oneToAllDijkstraElapsedTime);
+	}
+}
