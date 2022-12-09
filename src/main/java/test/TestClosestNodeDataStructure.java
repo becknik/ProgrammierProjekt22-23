a96ff2b2afa385c28a104f1b89d4237d06d73c3c
jarnnk <jannikb@posteo.de>
2022-12-09 15:37:47 +0100

    Try on first JUnit test

--- src/main/java/test/TestClosestNodeDataStructure.java
+++ src/main/java/test/TestClosestNodeDataStructure.java
@@ -1,4 +1,34 @@
 package test;
 
+import loader.GraphReader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import struct.AdjacencyGraph;
+import struct.ClosestNodeDataStructure;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestClosestNodeDataStructure {
+
+	AdjacencyGraph simpleAdjGraph;
+
+	@BeforeEach
+	public void setUpAdjGraphs () {
+		simpleAdjGraph = GraphReader.createAdjacencyGraphOf(new File("toy.fmi"));
+	}
+
+	@Test
+	public void test1 () {
+		System.out.println("Test!");
+		assertEquals(true, true);
+	}
+
+	@Test
+	public void simpleGetClosestNodeTest () {
+		ClosestNodeDataStructure closestNode = new ClosestNodeDataStructure(this.simpleAdjGraph);
+		ClosestNodeDataStructure.Node node = closestNode.getClosestNode(0, 0);
+		assertEquals(node.nodeId(), 0);
+	}
 }
