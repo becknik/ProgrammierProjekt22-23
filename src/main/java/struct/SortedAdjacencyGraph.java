e0e1e5dc6357bd863d533a42732893abd7f2e95a
TimExeler <st178137@stud.uni-stuttgart.de>
2022-12-15 17:36:58 +0100

    deleted code duplication and added javadoc

--- src/main/java/struct/SortedAdjacencyGraph.java
+++ src/main/java/struct/SortedAdjacencyGraph.java
@@ -47,7 +47,15 @@ public class SortedAdjacencyGraph {
 		}
 	}
 
-	private final Node[] sortedAdjacencyGraph;
+	/**
+	 * This is the enum is the collection for the possible direction to traverse an array.
+	 */
+	private enum MovementDirection {
+		RIGHT,
+		LEFT
+	}
+
+	private final IndexNode[] nodeIdsSortedByLatitude;
 
 	/**
 	 * Constructor for sortedAdjacencyGraph.
@@ -77,17 +85,79 @@ public class SortedAdjacencyGraph {
 	public Node getClosestNode (final double longitude, final double latitude) {
 		Node imaginaryNode = new Node(longitude, latitude, -1);
 		//find Index, where the imaginaryNode would be contained in sortedAdjacencyGraph, according to the latitude coordinate.
-		int nearestNodeIndex = this.getPivotIndex(imaginaryNode);
+		int colestNodeIndex = this.getNodeWithNearestLatitude(imaginaryNode);
 
-		//find both candidates for closest node to imaginary node by traversing sortedAdjacencyGraph to the right and left.
-		Node candidateOne = this.getCandidateTraversingRight(nearestNodeIndex, imaginaryNode);
-		double candidateOneDistance = candidateOne.getDistanceTo(imaginaryNode);
-		Node candidateTwo = this.getCandidateTraversingLeft(nearestNodeIndex, imaginaryNode);
-		double candidateTwoDistance = candidateTwo.getDistanceTo(imaginaryNode);
+		colestNodeIndex = this.getNearestNodeId(longitude, latitude, colestNodeIndex, MovementDirection.RIGHT);
+		colestNodeIndex = this.getNearestNodeId(longitude, latitude, colestNodeIndex, MovementDirection.LEFT);
 
-		Node closestNode = (candidateOneDistance <= candidateTwoDistance) ? candidateOne : candidateTwo;
+		return this.nodeIdsSortedByLatitude[colestNodeIndex];
+	}
 
-		return closestNode;
+	/**
+	 *
+	 * @param longitude the longitude value of the given position
+	 * @param latitude the latitude value of the given position
+	 * @param initialNearestNodeIndex the index of the Node, which has the lowest difference in latitude values to the given position
+	 * @param direction the direction in which nodeIdsSortedByLatitude will be looked at for the closest node to the given position
+	 * @return the index of the closest node to the given position in the "direction" side of sorted array of nodeIdsSortedByLatitude
+	 */
+	private int getNearestNodeId (final double longitude, final double latitude, final int initialNearestNodeIndex, final MovementDirection direction) {
+		final int crementer = (direction == MovementDirection.RIGHT) ? 1 : -1;
+
+		int currentBestIndex = initialNearestNodeIndex;
+		double currentBestDistance = this.calculateDistancesOf(longitude, latitude, initialNearestNodeIndex);
+
+		// For loop searches the left side of sorted array of initial node index for nodes with better distance to specified coords
+		for (int currentNodesIndex = initialNearestNodeIndex + crementer;
+		     !this.isOutOfBounds(currentNodesIndex) && !this.isLatitudeDiffGreater(currentBestDistance, latitude, currentNodesIndex);
+			 currentNodesIndex += crementer) {
+
+			double currentNodesDistance = this.calculateDistancesOf(longitude, latitude, currentNodesIndex);
+
+			if (currentNodesDistance < currentBestDistance) {
+				currentBestIndex = currentNodesIndex;
+				currentBestDistance = currentNodesDistance;
+			}
+		}
+
+		return currentBestIndex;
+	}
+
+	/**
+	 * This method calculates the distance between a given position and the node at a certain index in nodeIdsSortedByLatitude
+	 * @param fromLongitude the longitude value of the given position
+	 * @param fromLatitude the latitude value of the given position
+	 * @param toNodeIndex the index of the node to calculate the distance to
+	 * @return the distance between the position and the node at the index in nodeIdsSortedByLatitude
+	 */
+	private double calculateDistancesOf(final double fromLongitude, final double fromLatitude, final int toNodeIndex) {
+		return Math.sqrt(Math.pow((fromLongitude - nodeIdsSortedByLatitude[toNodeIndex].longitude), 2) + Math.pow((fromLatitude - this.nodeIdsSortedByLatitude[toNodeIndex].latitude), 2));
+	}
+
+	/**
+	 * This method calculates the determination condition for the for loop in getNearestNodeId method.
+	 * This method returns true if the latitude difference between fromLatitude and the latitude value of the node at toNodesIndex
+	 * is greater than the value of bestDistance
+	 * @param bestDistance the distance value which will be compared to the latitude difference
+	 * @param fromLatitude the latitude value of a given postition
+	 * @param toNodesIndex the index of the node which is looked at
+	 * @return true if the latitude difference is greater than the current best distance value
+	 */
+	private boolean isLatitudeDiffGreater(final double bestDistance, final double fromLatitude, final int toNodesIndex) {
+		double latitudeDif = Math.abs(fromLatitude - this.nodeIdsSortedByLatitude[toNodesIndex].latitude);
+
+		return (latitudeDif > bestDistance);
+	}
+
+	/**
+	 * This method calculates the determination condition for the for loop in getNearestNodeId method.
+	 * This method returns true if the given index is out of bounds for nodeIdsSortedByLatitude.
+	 * @param index the given index
+	 * @return true if index is out of bounds for nodeIdsSortedByLatitude, else false
+	 */
+	private boolean isOutOfBounds (final int index) {
+		if (index < 0 || this.nodeIdsSortedByLatitude.length <= index) return true;
+		else return false;
 	}
 
 	/**
@@ -157,7 +227,7 @@ public class SortedAdjacencyGraph {
 	 * @param node - The node with the lowest latitude will be searched for
 	 * @return - Index of node in the sorted adj graph which has the lowest difference in latitude
 	 */
-	public int getPivotIndex (Node node) {
+	public int getNodeWithNearestLatitude (IndexNode node) {
 		//Returns: index of the node, if it is contained in sortedAdjacencyGraph
 		//otherwise returns the negative index where should be
 		int indexOfSearchPoint = Arrays.binarySearch(this.sortedAdjacencyGraph, node);
